cb51c3c45af23c54ba89e943e47e91f56ecb2390
kigawa <contact@kigawa.net>
2024-04-17 09:39:47 +0000

    Add new server subcommand and update code formatting

--- core/src/main/kotlin/net/kigawa/oyu/scoreboardstore/command/ScoreStoreCommand.kt
+++ core/src/main/kotlin/net/kigawa/oyu/scoreboardstore/command/ScoreStoreCommand.kt
@@ -21,86 +21,99 @@ import java.util.concurrent.CompletableFuture
 
 @Kunit
 class ScoreStoreCommand(
-  private val scoreboardManager: ScoreboardManager,
-  private val database: Database,
-  private val pluginMessages: PluginMessages,
-  private val coroutines: Coroutines,
-  private val guiCommand: GuiCommand,
+    private val scoreboardManager: ScoreboardManager,
+    private val database: Database,
+    private val pluginMessages: PluginMessages,
+    private val coroutines: Coroutines,
+    private val guiCommand: GuiCommand,
 ) {
-  init {
-    val commandAPICommand =
-      CommandAPICommand("score-store")
-        .withPermission(CommandPermission.OP)
-    KutilReflect.getAllExitMethod(javaClass).forEach {
-      it.getAnnotation(SubCommand::class.java) ?: return@forEach
-      if (it.returnType != CommandAPICommand::class.java) return@forEach
+    init {
+        val commandAPICommand =
+            CommandAPICommand("score-store")
+                .withPermission(CommandPermission.OP)
+        KutilReflect.getAllExitMethod(javaClass).forEach {
+            it.getAnnotation(SubCommand::class.java) ?: return@forEach
+            if (it.returnType != CommandAPICommand::class.java) return@forEach
 
-      it.isAccessible = true
+            it.isAccessible = true
 
-      commandAPICommand.withSubcommand(it.invoke(this) as CommandAPICommand)
+            commandAPICommand.withSubcommand(it.invoke(this) as CommandAPICommand)
+        }
+
+        commandAPICommand.register()
     }
 
-    commandAPICommand.register()
-  }
+    @SubCommand
+    fun save(): CommandAPICommand = CommandAPICommand("save")
+        .withArguments(PlayerArgument("player"))
+        .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
+            CompletableFuture.supplyAsync {
+                return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
+                    it.name
+                }.toTypedArray()
+            }
+        }))
+        .executes(CommandExecutor { _: CommandSender, commandArguments: CommandArguments ->
+            val player = commandArguments.get("player") as Player
+            val key = commandArguments.get("scoreboard name") as String
+            database.getPlayerDatabase(player).save(key)
+        })
 
-  @SubCommand
-  fun save(): CommandAPICommand = CommandAPICommand("save")
-    .withArguments(PlayerArgument("player"))
-    .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
-      CompletableFuture.supplyAsync {
-        return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
-          it.name
-        }.toTypedArray()
-      }
-    }))
-    .executes(CommandExecutor { _: CommandSender, commandArguments: CommandArguments ->
-      val player = commandArguments.get("player") as Player
-      val key = commandArguments.get("scoreboard name") as String
-      database.getPlayerDatabase(player).save(key)
-    })
+    @SubCommand
+    fun load(): CommandAPICommand = CommandAPICommand("load")
+        .withArguments(PlayerArgument("player"))
+        .withArguments(IntegerArgument("default value"))
+        .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
+            CompletableFuture.supplyAsync {
+                return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
+                    it.name
+                }.toTypedArray()
+            }
+        }))
+        .executes(CommandExecutor { _: CommandSender, commandArguments: CommandArguments ->
+            val player = commandArguments.get("player") as Player
+            val key = commandArguments.get("scoreboard name") as String
+            val defaultValue = commandArguments.get("default value") as Int
+            database.getPlayerDatabase(player).load(key, defaultValue)
+        })
 
-  @SubCommand
-  fun load(): CommandAPICommand = CommandAPICommand("load")
-    .withArguments(PlayerArgument("player"))
-    .withArguments(IntegerArgument("default value"))
-    .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
-      CompletableFuture.supplyAsync {
-        return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
-          it.name
-        }.toTypedArray()
-      }
-    }))
-    .executes(CommandExecutor { _: CommandSender, commandArguments: CommandArguments ->
-      val player = commandArguments.get("player") as Player
-      val key = commandArguments.get("scoreboard name") as String
-      val defaultValue = commandArguments.get("default value") as Int
-      database.getPlayerDatabase(player).load(key, defaultValue)
-    })
+    @SubCommand
+    fun playerCount(): CommandAPICommand = CommandAPICommand("player-count")
+        .withArguments(PlayerArgument("player"))
+        .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
+            CompletableFuture.supplyAsync {
+                return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
+                    it.name
+                }.toTypedArray()
+            }
+        }))
+        .withArguments(StringArgument("server name"))
+        .executes(CommandExecutor { _, commandArguments ->
+            val scoreBoard = commandArguments.get("scoreboard name") as String
+            val serverName = commandArguments.get("server name") as String
+            val player = commandArguments.get("player") as Player
 
-  @SubCommand
-  fun playerCount(): CommandAPICommand = CommandAPICommand("player-count")
-    .withArguments(PlayerArgument("player"))
-    .withArguments(StringArgument("scoreboard name").replaceSuggestions(ArgumentSuggestions.stringsAsync {
-      CompletableFuture.supplyAsync {
-        return@supplyAsync scoreboardManager.mainScoreboard.objectives.map {
-          it.name
-        }.toTypedArray()
-      }
-    }))
-    .withArguments(StringArgument("server name"))
-    .executes(CommandExecutor { _, commandArguments ->
-      val scoreBoard = commandArguments.get("scoreboard name") as String
-      val serverName = commandArguments.get("server name") as String
-      val player = commandArguments.get("player") as Player
+            scoreboardManager.mainScoreboard.getScores(player.name).first { it.objective.name == scoreBoard }.score = -1
+            coroutines.launchDefault {
+                val count = pluginMessages.getPlayerCount(player, serverName).await()
+                scoreboardManager.mainScoreboard.getScores(player.name)
+                    .first { it.objective.name == scoreBoard }.score = count
+            }
+        })
 
-      scoreboardManager.mainScoreboard.getScores(player.name).first { it.objective.name == scoreBoard }.score = -1
-      coroutines.launchDefault {
-        val count = pluginMessages.getPlayerCount(player, serverName).await()
-        println(count)
-        scoreboardManager.mainScoreboard.getScores(player.name).first { it.objective.name == scoreBoard }.score = count
-      }
-    })
+    @SubCommand
+    fun server(): CommandAPICommand = CommandAPICommand("player-count")
+        .withArguments(PlayerArgument("player"))
+        .withArguments(StringArgument("server name"))
+        .executes(CommandExecutor { _, commandArguments ->
+            val serverName = commandArguments.get("server name") as String
+            val player = commandArguments.get("player") as Player
 
-  @SubCommand
-  fun gui() = guiCommand.cmd()
+            coroutines.launchDefault {
+                pluginMessages.connectServer(player, serverName).await()
+            }
+        })
+
+    @SubCommand
+    fun gui() = guiCommand.cmd()
 }
--- core/src/main/kotlin/net/kigawa/oyu/scoreboardstore/pluginmessage/PluginMessages.kt
+++ core/src/main/kotlin/net/kigawa/oyu/scoreboardstore/pluginmessage/PluginMessages.kt
@@ -17,87 +17,91 @@ import java.io.DataOutputStream
 
 @Kunit
 class PluginMessages(
-  private val server: Server,
-  private val plugin: PluginBase,
-  private val coroutines: Coroutines,
+    private val server: Server,
+    private val plugin: PluginBase,
+    private val coroutines: Coroutines,
 ) : PluginMessageListener, AutoCloseable {
-  private val receiveChannel = Channel<Int>(1)
-  private val taskChannel = Channel<suspend () -> Unit>()
+    private val receiveChannel = Channel<Int>(1)
+    private val taskChannel = Channel<suspend () -> Unit>()
 
-  init {
-    server.messenger.registerOutgoingPluginChannel(plugin, "BungeeCord");
-    server.messenger.registerIncomingPluginChannel(plugin, "BungeeCord", this);
-    coroutines.launchDefault {
-      for (task in taskChannel) {
-        try {
-          task.invoke()
-        } catch (e: Exception) {
-          e.printStackTrace()
+    init {
+        server.messenger.registerOutgoingPluginChannel(plugin, "BungeeCord")
+        server.messenger.registerIncomingPluginChannel(plugin, "BungeeCord", this)
+        coroutines.launchDefault {
+            for (task in taskChannel) {
+                try {
+                    task.invoke()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            receiveChannel.close()
+            server.messenger.unregisterOutgoingPluginChannel(plugin)
+            server.messenger.unregisterIncomingPluginChannel(plugin)
         }
-      }
-      receiveChannel.close()
-      server.messenger.unregisterOutgoingPluginChannel(plugin);
-      server.messenger.unregisterIncomingPluginChannel(plugin);
     }
-  }
 
 
-  private suspend fun <T> runTask(
-    player: Player,
-    vararg send: String,
-    receive: suspend () -> T,
-  ): Deferred<T> {
-    val res = coroutines.async(start = CoroutineStart.LAZY) {
-      val byteOut = ByteArrayOutputStream()
-      val out = DataOutputStream(byteOut)
-      send.forEach {
-        out.writeUTF(it)
-      }
-      player.sendPluginMessage(plugin, "BungeeCord", byteOut.toByteArray())
+    private suspend fun <T> runTask(
+        player: Player,
+        vararg send: String,
+        receive: suspend () -> T,
+    ): Deferred<T> {
+        val res = coroutines.async(start = CoroutineStart.LAZY) {
+            val byteOut = ByteArrayOutputStream()
+            val out = DataOutputStream(byteOut)
+            send.forEach {
+                out.writeUTF(it)
+            }
+            player.sendPluginMessage(plugin, "BungeeCord", byteOut.toByteArray())
 
-      receive.invoke()
-    }
-    taskChannel.send {
-      res.start()
-      res.await()
-    }
-    return res
-  }
-
-  override fun close() {
-    taskChannel.close()
-  }
-
-  override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
-    coroutines.launchDefault {
-      var result = 0
-      try {
-        if (channel != "BungeeCord") {
-          result = -2
-          return@launchDefault
+            receive.invoke()
         }
-        val byteIn = DataInputStream(ByteArrayInputStream(message))
-
-        when (val sub = byteIn.readUTF()) {
-          "PlayerCount" -> {
-            byteIn.readUTF()
-            result = byteIn.readInt()
-          }
-
-          else -> {
-            println(sub)
-            result = -3
-          }
+        taskChannel.send {
+            res.start()
+            res.await()
         }
-      } finally {
-        receiveChannel.send(result)
-      }
+        return res
     }
-  }
+
+    override fun close() {
+        taskChannel.close()
+    }
+
+    override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
+        coroutines.launchDefault {
+            var result = 0
+            try {
+                if (channel != "BungeeCord") {
+                    result = -2
+                    return@launchDefault
+                }
+                val byteIn = DataInputStream(ByteArrayInputStream(message))
+
+                when (val sub = byteIn.readUTF()) {
+                    "PlayerCount" -> {
+                        byteIn.readUTF()
+                        result = byteIn.readInt()
+                    }
+
+                    else -> {
+                        println(sub)
+                        result = -3
+                    }
+                }
+            } finally {
+                receiveChannel.send(result)
+            }
+        }
+    }
 
 
-  suspend fun getPlayerCount(player: Player, server: String) = runTask(player, "PlayerCount", server) {
-    return@runTask receiveChannel.receive()
-  }
+    suspend fun getPlayerCount(player: Player, server: String) = runTask(player, "PlayerCount", server) {
+        return@runTask receiveChannel.receive()
+    }
+
+    suspend fun connectServer(player: Player, server: String) = runTask(player, "Connect", server) {
+        return@runTask receiveChannel.receive()
+    }
 
 }
